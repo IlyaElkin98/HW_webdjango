6849dcb93b6ac6cc5068c45d1fbcc3dd8cb06c4b
ilya <ilifud98@mail.ru>
2025-12-15 23:26:14 +0700

    Создан файл models.py

--- product/models.py
+++ product/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+class Product(models.Model):
+    name = models.CharField(
+        max_length=150,
+        verbose_name="Название",
+        help_text="Введите название продукта",
+    )
+
+    description = models.TextField(
+        verbose_name="Описание",
+        help_text="Введите описание",
+    )
+
+    price = models.IntegerField(
+        verbose_name="Цена",
+        max_length=30,
+        help_text="Введите цену"
+    )
+
