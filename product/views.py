802b9d755e17d3ceac190d51aa8cffaedd92d6d3
ilya <ilifud98@mail.ru>
2025-12-15 23:11:05 +0700

    настройка контроллера и маршутизациию Не полная

--- product/views.py
+++ product/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
-
+from django.urls import reverse, reverse_lazy
+from django.views.generic import ListView, DetailView
+from django.views.generic.edit import CreateView, DeleteView, UpdateView
+from product.models import Product
 # Create your views here.
