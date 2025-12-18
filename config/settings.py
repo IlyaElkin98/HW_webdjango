4d53a0140151883d015fbe078975da1c8985583d
ilya <ilifud98@mail.ru>
2025-12-19 00:05:57 +0700

    Сделаны задания по чек-листу до миграции

--- config/settings.py
+++ config/settings.py
@@ -19,7 +19,9 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+
     "product",
+    "user",
 ]
 
 MIDDLEWARE = [
--- user/models.py
+++ user/models.py
@@ -1,3 +1,16 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+class User(AbstractUser):
+    username = None
+    email = models.EmailField(unique=True, verbose_name="Email")
+    avatar = models.ImageField(upload_to="user/avatars/" ,width_field=150, height_field=150, verbose_name="Аватар", help_text="Загрузите аватар", null=True, blank=True)
+    phone_number = models.CharField(blank=True, help_text="Укажите номер телефона", null=True)
+    country = models.CharField(max_length=50, verbose_name="Страна", help_text="Укажите страну")
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    class Meta:
+        verbose_name = "user"
+        verbose_name_plural = "users"
