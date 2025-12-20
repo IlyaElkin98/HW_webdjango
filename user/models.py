c9888beb63c54a04e8ba77a4979b6fd865e16188
ilya <ilifud98@mail.ru>
2025-12-20 23:24:40 +0700

    реализована полностью 3 задача

--- user/models.py
+++ user/models.py
@@ -6,7 +6,7 @@ from django.db import models
 class User(AbstractUser):
     username = None
     email = models.EmailField(unique=True, verbose_name="Email")
-    avatar = models.ImageField(upload_to="user/avatars/" ,width_field=150, height_field=150, verbose_name="Аватар", help_text="Загрузите аватар", null=True, blank=True)
+    avatar = models.ImageField(upload_to="media/user" ,width_field=150, height_field=150, verbose_name="Аватар", help_text="Загрузите аватар", null=True, blank=True)
     phone_number = models.CharField(blank=True, help_text="Укажите номер телефона", null=True)
     country = models.CharField(max_length=50, verbose_name="Страна", help_text="Укажите страну", blank=True, null=True)
     token = models.CharField(max_length=100, verbose_name='Token', blank=True, null=True)
