1fb38d393e55a5cb23713d77d4d79f49a7fc54c1
ilya <ilifud98@mail.ru>
2025-12-22 16:19:16 +0700

    refactor:(user.admin) add registration user

--- user/admin.py
+++ user/admin.py
@@ -1,8 +1,8 @@
-# from django.contrib import admin
-# from .models import User
-#
-# @admin.register(User)
-# class UserAdmin(admin.ModelAdmin):
-#     list_display = ["id", "username", "email", "avatar", "phone_number", "country"]
-#     list_filter = ("id",)
-#     search_fields = ("username",)
+from django.contrib import admin
+from .models import User
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ["id", "username", "email", "avatar", "phone_number", "country"]
+    list_filter = ("id",)
+    search_fields = ("username",)
