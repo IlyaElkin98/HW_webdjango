80ccb7fcc960b2b091a68c5ea0798452175c4d3f
ilya <ilifud98@mail.ru>
2025-12-22 20:57:52 +0700

    refactor:(product.views) add functionality of access rights for deleting and unpublishing a product has been implemented.

--- product/views.py
+++ product/views.py
@@ -1,6 +1,9 @@
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.decorators import permission_required
+from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
+from django.http import HttpResponseForbidden
+from django.shortcuts import get_object_or_404, redirect
 from django.urls import reverse_lazy,reverse
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, View
 from django.views.generic.edit import CreateView, DeleteView, UpdateView
 from product.models import Product
 from product.forms import ProductForm
@@ -30,8 +33,25 @@ class ProductDetailView(LoginRequiredMixin, DetailView):
     template_name = "product/product_detail.html"
     context_object_name = "product"
 
-class ProductDeleteView(LoginRequiredMixin, DeleteView):
+class ProductDeleteView(LoginRequiredMixin, PermissionRequiredMixin, DeleteView):
     model = Product
     template_name = "product/product_confirm_delete.html"
     success_url = reverse_lazy("product:products")
+    permission_required = 'product.delete_product'
+
+    def delete(self, request, *args, **kwargs):
+        if not request.user.has_perm(self.permission_required):
+            return HttpResponseForbidden('У вас нет прав на удаление продуктов')
+        return super().delete(request, *args, **kwargs)
+
+class ProductPublicationStatusView(LoginRequiredMixin, View):
+    def post(self, request, pk):
+        product = get_object_or_404(Product, id=pk)
+
+        if not request.user.has_perm('product.can_unpublish_product'):
+            return HttpResponseForbidden('У вас нет прав на снятия публикации продукта')
+
+        product.publication_status = True
+        product.save()
+        return redirect('product:products')
 
